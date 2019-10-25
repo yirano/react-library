71d34fd4c2525df4eca1c9dbc2c71ba905f4e616
Newname <newemail>
2019-10-25 11:15:45 -0400

    Add delete button to cards

--- src/App.js
+++ src/App.js
@@ -7,7 +7,23 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      books: ['one', 'two', 'three']
+      books: [
+        {
+          title: 'What if?',
+          author: 'Randall Munroe',
+          id: 1
+        },
+        {
+          title: 'Woolly',
+          author: 'Ben Mezrich',
+          id: 2
+        },
+        {
+          title: 'The Dovekeepers',
+          author: 'Alice Hoffman',
+          id: 3
+        }
+      ]
     }
   }
   render(){
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,14 @@ import './card-list.styles.css';
 import {Card} from '../card/card.component'
 
 export const CardList = (props) => (
-  <div>
-    <Card />
+  <div className='card-list'>
+    <div className ='card-list-bar'> 
+      <p className ='bar-title'>Title</p>
+      <p className ='bar-author'>Author</p>
+      <p className ='bar-pages'>Pages</p>
+    </div>    
+    {props.books.map(book=> (
+      <Card key={book.id} book={book}/>
+    ))}
   </div>
 )
