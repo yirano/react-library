deebe129cccf403b860a9c4524087e12376a5ce6
Newname <newemail>
2019-10-25 11:39:27 -0400

    Start on the beginning fuctions of buttons

--- src/components/input/input.component.jsx
+++ src/components/input/input.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './input.styles.css';
 
+function addToLibrary() {
+  console.log('Add to library');
+}
+
 export const Input = props => (
   <div className = "inputBox">
     <input type="text" name="input-title" id="input-title" placeholder="Title" />
     <input type="text" name="input-author" id="input-author" placeholder="Author" />
     <input type="number" name="input-pages" id="input-pages" placeholder="Pages" />
-    <button>Submit</button>
+    <button className = 'input-button' onClick = {addToLibrary}>Submit</button>
   </div>
 )
