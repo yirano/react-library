adb14025831a473c487e9f4dbb2a872e23523008
Newname <newemail>
2019-10-25 11:28:36 -0400

    Add input box for adding into the library

--- src/components/input/input.component.jsx
+++ src/components/input/input.component.jsx
@@ -3,6 +3,9 @@ import './input.styles.css';
 
 export const Input = props => (
   <div className = "inputBox">
-    <input type="text" name="" id="input" placeholder="Add to your library" />
+    <input type="text" name="input-title" id="input-title" placeholder="Title" />
+    <input type="text" name="input-author" id="input-author" placeholder="Author" />
+    <input type="number" name="input-pages" id="input-pages" placeholder="Pages" />
+    <button>Submit</button>
   </div>
 )
