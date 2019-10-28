4519601c4fc8a23952c7160b7e44bfad54562abc
yirano <yirano@protonmail.ch>
2019-10-28 09:40:29 -0400

    Remove books array from App.js and make separate file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React from 'react';
-import {CardList} from './components/card-list/card-list.component';
-import ToggleInput from './components/toggle-input/toggle-input.component';
-import './App.css';
+import React from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import ToggleInput from "./components/toggle-input/toggle-input.component";
+import "./App.css";
+import books from "./components/library/library";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: [
-        {
-          title: 'What if?',
-          author: 'Randall Munroe',
-          id: 1
-        },
-        {
-          title: 'Woolly',
-          author: 'Ben Mezrich',
-          id: 2
-        },
-        {
-          title: 'The Dovekeepers',
-          author: 'Alice Hoffman',
-          id: 3
-        }
-      ]
-    }
+      books: books
+    };
   }
-  render(){
+  render() {
     return (
-      <div className = "container">
+      <div className="container">
         <ToggleInput />
         <CardList books={this.state.books} />
       </div>
-    )
+    );
   }
 }
 
---
+++ src/components/library/library.js
@@ -0,0 +1,19 @@
+const books = [
+  {
+    title: "What if?",
+    author: "Randall Munroe",
+    id: 1
+  },
+  {
+    title: "Woolly",
+    author: "Ben Mezrich",
+    id: 2
+  },
+  {
+    title: "The Dovekeepers",
+    author: "Alice Hoffman",
+    id: 3
+  }
+];
+
+export default books;
